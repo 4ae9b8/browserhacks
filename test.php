e30d2d4268f3f4da0951aa2db2e91f5107a0a477
Tim Pietrusky <timpietrusky@googlemail.com>
2013-02-15 22:56:48 +0100

    Minor improvements.

--- test.php
+++ test.php
@@ -430,8 +430,7 @@
         </section>
     </article>  
 
-    <!--<script src="http://code.jquery.com/jquery-1.9.0.min.js"></script>-->
-    <script src="js/jquery-1.9.0-min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
     <script src="js/browserhacks-tests.js"></script>
 
     <script type="text/javascript">
