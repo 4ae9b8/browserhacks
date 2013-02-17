9ec300dfa7c719a4db91fd2f67be092b6cf88b64
Tim Pietrusky <timpietrusky@googlemail.com>
2013-02-17 19:56:51 +0100

    #8: Added "selectivizr.js" and "html5shiv.js" to enhance IE compatiblity.

--- index.php
+++ index.php
@@ -170,6 +170,11 @@
     <script src="js/libs.js"></script>
     <script src="js/main.js"></script>
 
+    <!--[if (gte IE 6)&(lte IE 8)]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script type="text/javascript" src="js/selectivizr-min.js"></script>
+    <![endif]-->
+
     <?php if (Browserhacks::isLive()): ?>
         <script type="text/javascript">var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-38522111-1']);_gaq.push(['_setDomainName', 'browserhacks.com']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
     <?php endif; ?>
