d1af4077480a2c38ff0a73143ba71f025fbf1c64
Tim Pietrusky <timpietrusky@googlemail.com>
2013-05-30 21:27:15 +0200

    Added a minify bash script.

---
+++ js/minify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for jsFile in "$@"
+do
+    simpleName=${jsFile:0:${#jsFile}-3} 
+
+    # If the output file already exists, we don't want to double its size. Remove it.
+    if [ -e "$simpleName-min.js" ]; then
+      echo -e "Removing existing copy of $simpleName-min.js."
+      rm "$simpleName-min.js"
+    fi
+
+    yui-compressor -o "$simpleName-min.js" "$simpleName.js"
+    echo -e "Created $simpleName-min.js"
+
+done
+
+exit 0
