f7360ba3f044090c7759bfeaa938ab21c17e28b3
Tim Pietrusky <timpietrusky@googlemail.com>
2013-08-10 14:22:48 +0200

    Improved message ".nothing".

--- js/minify.sh
+++ js/minify.sh
@@ -3,7 +3,7 @@
 for jsFile in "$@"
 do
     simpleName=${jsFile:0:${#jsFile}-3} 
-
+echo -e $simpleName
     # If the output file already exists, we don't want to double its size. Remove it.
     if [ -e "$simpleName-min.js" ]; then
       echo -e "Removing existing copy of $simpleName-min.js."
@@ -12,7 +12,6 @@ do
 
     yui-compressor -o "$simpleName-min.js" "$simpleName.js"
     echo -e "Created $simpleName-min.js"
-
 done
 
 exit 0
