a073423eabb71124bc15c629b13f784b6d978b36
Tim Pietrusky <timpietrusky@googlemail.com>
2013-08-11 12:55:30 +0200

    #91: Improved auto-pull script.

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start the ssh-agent
+eval `ssh-agent -s` 
+
+# Add ssh key
+ssh-add /root/.ssh/id_rsa.pub
+
+# Get changes from repo
+git pull origin master
+
+# Kill the ssh-agent
+kill $SSH_AGENT_PID
