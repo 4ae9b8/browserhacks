16bc84a850e2c923d329ff1c7099713536fbd7c7
Tim Pietrusky <timpietrusky@googlemail.com>
2013-08-12 19:16:21 +0200

    Optimized the JS for the testpage.

--- test.php
+++ test.php
@@ -96,15 +96,13 @@
             </div>
         </section>
     </article>
-
-    <script src="js/libs.js"></script>
-    <!--<script src="js/lib/browserdetect-min.js"></script>-->
+    
+    <script src="js/main.testpage.js"></script> 
     <script src="js/browserhacks-test-page.js"></script>
+
     <?php if (Browserhacks::isLive()): ?>
-        <script src="js/main.min.js"></script> 
         <script type="text/javascript">var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-38522111-1']);_gaq.push(['_setDomainName', 'browserhacks.com']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
-    <?php else: ?>
-        <script src="js/main.js"></script>
     <?php endif; ?>
+
     </body>
 </html>
