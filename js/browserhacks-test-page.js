2dcfee5c1f7412320a10c7c20075f0afab654fbb
Hugo Giraudel <hugo.giraudel@gmail.com>
2013-09-04 17:18:48 +0200

    Try to fix test page bug

--- js/browserhacks-test-page.js
+++ js/browserhacks-test-page.js
@@ -1,90 +1,90 @@
-var testClass = 'js-succeed';
+var testClass = 'js-succeed';  
 var test_js_an_4 = !!window.chrome;
-if (test_js_an_4) $('.test_js_an_4').addClass(testClass);
+if (test_js_an_4) $('.test_js_an_4').addClass(testClass);
 var test_js_ch_9 = !!window.chrome && !!window.chrome.webstore;
-if (test_js_ch_9) $('.test_js_ch_9').addClass(testClass);
+if (test_js_ch_9) $('.test_js_ch_9').addClass(testClass);
 var test_js_ch_11 = !!window.chrome;
-if (test_js_ch_11) $('.test_js_ch_11').addClass(testClass);
+if (test_js_ch_11) $('.test_js_ch_11').addClass(testClass);
 var test_js_ch_13 = 'WebkitAppearance' in document.documentElement.style;
-if (test_js_ch_13) $('.test_js_ch_13').addClass(testClass);
+if (test_js_ch_13) $('.test_js_ch_13').addClass(testClass);
 var test_js_fx_22 = !!navigator.userAgent.match(/firefox/i);
-if (test_js_fx_22) $('.test_js_fx_22').addClass(testClass);
+if (test_js_fx_22) $('.test_js_fx_22').addClass(testClass);
 var test_js_fx_24 = !!window.sidebar;
-if (test_js_fx_24) $('.test_js_fx_24').addClass(testClass);
+if (test_js_fx_24) $('.test_js_fx_24').addClass(testClass);
 var test_js_fx_26 = 'MozAppearance' in document.documentElement.style;
-if (test_js_fx_26) $('.test_js_fx_26').addClass(testClass);
+if (test_js_fx_26) $('.test_js_fx_26').addClass(testClass);
 var test_js_fx_28 = typeof InstallTrigger !== 'undefined';
-if (test_js_fx_28) $('.test_js_fx_28').addClass(testClass);
+if (test_js_fx_28) $('.test_js_fx_28').addClass(testClass);
 var test_js_fx_30 = (function x(){})[-6]=='x';
-if (test_js_fx_30) $('.test_js_fx_30').addClass(testClass);
+if (test_js_fx_30) $('.test_js_fx_30').addClass(testClass);
 var test_js_fx_32 = /a/[-1]=='a';
-if (test_js_fx_32) $('.test_js_fx_32').addClass(testClass);
+if (test_js_fx_32) $('.test_js_fx_32').addClass(testClass);
 var test_js_fx_34 = !!window.globalStorage;
-if (test_js_fx_34) $('.test_js_fx_34').addClass(testClass);
+if (test_js_fx_34) $('.test_js_fx_34').addClass(testClass);
 var test_js_fx_36 = (function x(){})[-5]=='x';
-if (test_js_fx_36) $('.test_js_fx_36').addClass(testClass);
+if (test_js_fx_36) $('.test_js_fx_36').addClass(testClass);
 var test_js_ie_64 = (function() { if (new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null) { return parseFloat( RegExp.$1 ); } else { return false; } })();
-if (test_js_ie_64) $('.test_js_ie_64').addClass(testClass);
+if (test_js_ie_64) $('.test_js_ie_64').addClass(testClass);
 var test_js_ie_66 = document.all && window.atob;
-if (test_js_ie_66) $('.test_js_ie_66').addClass(testClass);
+if (test_js_ie_66) $('.test_js_ie_66').addClass(testClass);
 var test_js_ie_68 = eval("/*@cc_on!@*/false") && document.documentMode === 10;
-if (test_js_ie_68) $('.test_js_ie_68').addClass(testClass);
+if (test_js_ie_68) $('.test_js_ie_68').addClass(testClass);
 var test_js_ie_70 = 'behavior' in document.documentElement.style && '-ms-user-select' in document.documentElement.style;
-if (test_js_ie_70) $('.test_js_ie_70').addClass(testClass);
+if (test_js_ie_70) $('.test_js_ie_70').addClass(testClass);
 var test_js_ie_72 = document.body.style.msTouchAction !== undefined;
-if (test_js_ie_72) $('.test_js_ie_72').addClass(testClass);
+if (test_js_ie_72) $('.test_js_ie_72').addClass(testClass);
 var test_js_ie_74 = window.navigator.msPointerEnabled;
-if (test_js_ie_74) $('.test_js_ie_74').addClass(testClass);
+if (test_js_ie_74) $('.test_js_ie_74').addClass(testClass);
 var test_js_ie_76 = /*@cc_on!@*/false;
-if (test_js_ie_76) $('.test_js_ie_76').addClass(testClass);
+if (test_js_ie_76) $('.test_js_ie_76').addClass(testClass);
 var test_js_ie_78 = '-ms-scroll-limit' in document.documentElement.style && '-ms-ime-align' in document.documentElement.style;
-if (test_js_ie_78) $('.test_js_ie_78').addClass(testClass);
+if (test_js_ie_78) $('.test_js_ie_78').addClass(testClass);
 var test_js_ie_80 = /*@cc_on (function() {switch(@_jscript_version) {case 1.0: return 3; case 3.0: return 4; case 5.0: return 5; case 5.1: return 5; case 5.5: return 5.5; case 5.6: return 6; case 5.7: return 7; case 5.8: return 8; case 9: return 9; case 10: return 10;}})() || @*/ 0;
-if (test_js_ie_80) $('.test_js_ie_80').addClass(testClass);
+if (test_js_ie_80) $('.test_js_ie_80').addClass(testClass);
 var test_js_ie_82 = document.all && !window.XMLHttpRequest;
-if (test_js_ie_82) $('.test_js_ie_82').addClass(testClass);
+if (test_js_ie_82) $('.test_js_ie_82').addClass(testClass);
 var test_js_ie_84 = navigator.appVersion.indexOf("MSIE 7.") !== -1;
-if (test_js_ie_84) $('.test_js_ie_84').addClass(testClass);
+if (test_js_ie_84) $('.test_js_ie_84').addClass(testClass);
 var test_js_ie_86 = document.all && document.querySelector && !document.addEventListener;
-if (test_js_ie_86) $('.test_js_ie_86').addClass(testClass);
+if (test_js_ie_86) $('.test_js_ie_86').addClass(testClass);
 var test_js_ie_88 = !+'\v1';
-if (test_js_ie_88) $('.test_js_ie_88').addClass(testClass);
+if (test_js_ie_88) $('.test_js_ie_88').addClass(testClass);
 var test_js_ie_90 = '\v'=='v';
-if (test_js_ie_90) $('.test_js_ie_90').addClass(testClass);
+if (test_js_ie_90) $('.test_js_ie_90').addClass(testClass);
 var test_js_ie_92 = document.all && document.querySelector;
-if (test_js_ie_92) $('.test_js_ie_92').addClass(testClass);
+if (test_js_ie_92) $('.test_js_ie_92').addClass(testClass);
 var test_js_ie_94 = document.all && document.addEventListener && !window.atob;
-if (test_js_ie_94) $('.test_js_ie_94').addClass(testClass);
+if (test_js_ie_94) $('.test_js_ie_94').addClass(testClass);
 var test_js_ie_96 = document.all && !window.atob;
-if (test_js_ie_96) $('.test_js_ie_96').addClass(testClass);
+if (test_js_ie_96) $('.test_js_ie_96').addClass(testClass);
 var test_js_ie_98 = document.all && !document.addEventListener;
-if (test_js_ie_98) $('.test_js_ie_98').addClass(testClass);
+if (test_js_ie_98) $('.test_js_ie_98').addClass(testClass);
 var test_js_ie_100 = document.all && document.addEventListener;
-if (test_js_ie_100) $('.test_js_ie_100').addClass(testClass);
+if (test_js_ie_100) $('.test_js_ie_100').addClass(testClass);
 var test_js_ie_102 = document.all && document.compatMode;
-if (test_js_ie_102) $('.test_js_ie_102').addClass(testClass);
+if (test_js_ie_102) $('.test_js_ie_102').addClass(testClass);
 var test_js_ie_104 = document.all && window.XMLHttpRequest && !document.addEventListener;
-if (test_js_ie_104) $('.test_js_ie_104').addClass(testClass);
+if (test_js_ie_104) $('.test_js_ie_104').addClass(testClass);
 var test_js_ie_106 = document.all && !document.querySelector;
-if (test_js_ie_106) $('.test_js_ie_106').addClass(testClass);
+if (test_js_ie_106) $('.test_js_ie_106').addClass(testClass);
 var test_js_ie_108 = document.all && window.XMLHttpRequest;
-if (test_js_ie_108) $('.test_js_ie_108').addClass(testClass);
+if (test_js_ie_108) $('.test_js_ie_108').addClass(testClass);
 var test_js_op_118 = !!window.opera;
-if (test_js_op_118) $('.test_js_op_118').addClass(testClass);
+if (test_js_op_118) $('.test_js_op_118').addClass(testClass);
 var test_js_op_120 = window.opera && window.opera.version() == X;
-if (test_js_op_120) $('.test_js_op_120').addClass(testClass);
+if (test_js_op_120) $('.test_js_op_120').addClass(testClass);
 var test_js_op_122 = /^function \(/.test([].sort);
-if (test_js_op_122) $('.test_js_op_122').addClass(testClass);
+if (test_js_op_122) $('.test_js_op_122').addClass(testClass);
 var test_js_op_124 = !!window.chrome;
-if (test_js_op_124) $('.test_js_op_124').addClass(testClass);
+if (test_js_op_124) $('.test_js_op_124').addClass(testClass);
 var test_js_op_126 = 'WebkitAppearance' in document.documentElement.style;
-if (test_js_op_126) $('.test_js_op_126').addClass(testClass);
+if (test_js_op_126) $('.test_js_op_126').addClass(testClass);
 var test_js_sa_136 = /a/.__proto__=='//';
-if (test_js_sa_136) $('.test_js_sa_136').addClass(testClass);
+if (test_js_sa_136) $('.test_js_sa_136').addClass(testClass);
 var test_js_sa_138 = /Constructor/.test(window.HTMLElement);
-if (test_js_sa_138) $('.test_js_sa_138').addClass(testClass);
+if (test_js_sa_138) $('.test_js_sa_138').addClass(testClass);
 var test_js_sa_140 = !!navigator.userAgent.match(/safari/i) && !navigator.userAgent.match(/chrome/i) && typeof document.body.style.webkitFilter !== "undefined";
-if (test_js_sa_140) $('.test_js_sa_140').addClass(testClass);
+if (test_js_sa_140) $('.test_js_sa_140').addClass(testClass);
 var test_js_sa_142 = 'WebkitAppearance' in document.documentElement.style;
-if (test_js_sa_142) $('.test_js_sa_142').addClass(testClass);
+if (test_js_sa_142) $('.test_js_sa_142').addClass(testClass);
 
