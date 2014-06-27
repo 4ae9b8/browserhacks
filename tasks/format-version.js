bf53f7481f95cb18618848d6b3f141bd3382aef1
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-06-27 19:17:00 +0200

    Solving #132

--- tasks/format-version.js
+++ tasks/format-version.js
@@ -2,25 +2,34 @@ var formatVersion = function(humanVersion){
   var version = humanVersion.split('|'),
       result  = humanVersion;
 
-  if (version.length > 1){
+  if (version.length > 1) {
     var continuous = true;
 
-    for (var i=0;i< version.length-1;i++){
+    for (var i = 0; i < version.length - 1; i++) {
       var thisV = parseFloat(version[i], 10),
-          nextV = parseFloat(version[i+1],10);
+          nextV = parseFloat(version[i + 1], 10);
 
-      if (thisV+1 !== nextV){
+      if (thisV + 1 !== nextV) {
         continuous = false;
         break;
       }
 
     }
 
-    if (continuous)
+    if (continuous) {
       result = version[0] + "-" + version[version.length-1];
+    }
   }
 
   result = result.replace(/\|/g, "/");
+
+  if (result[result.length - 1] === "+") {
+    result = '≥ ' + result.substring(0, result.length - 1);
+  }
+
+  else if (result[result.length - 1] === "-") {
+    result = '≤ ' + result.substring(0, result.length - 1);
+  }
 
   return result;
 };
