4677fbe7a9f813befe3e2b58366656612e10b9f9
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-04-10 16:07:13 +0200

    Removed safe flag from database

--- _bin/buildDatabase.js
+++ _bin/buildDatabase.js
@@ -6,16 +6,16 @@ var path = require('path')
 var addId = require('./addId')
 var addVersion = require('./addVersion')
 var addLegacyFlag = require('./addLegacyFlag')
-var addCssValidity = require('./addCssValidity')
-var addJsValidity = require('./addJsValidity')
+// var addCssValidity = require('./addCssValidity')
+// var addJsValidity = require('./addJsValidity')
 
 var FILE = path.resolve(__dirname, '..', '_data/hacks.json')
 var hacks = require(FILE).map((hack) => {
   hack = addId(hack)
   hack = addVersion(hack)
   hack = addLegacyFlag(hack)
-  hack = addCssValidity(hack)
-  hack = addJsValidity(hack)
+  // hack = addCssValidity(hack)
+  // hack = addJsValidity(hack)
 
   return hack
 })
